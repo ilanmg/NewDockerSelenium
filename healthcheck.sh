d22489ef2e09736e02769770eb05b111e790f71c
IlanMg <ilanmg@artlist.io>
2019-02-27 16:39:37 +0200

    finish

--- healthcheck.sh
+++ healthcheck.sh
@@ -1,13 +1,15 @@
+
+
+
 #!/usr/bin/env bash
 # Environment Variables
 # HUB_HOST
 # BROWSER
 # MODULE
 
-echo "Checking if hub is ready - $HUB_HOST"
+echo "Checking if hub is ready now - $HUB_HOST"
 
-while [ "$( curl -s http://$HUB_HOST:4444/wd/hub/status | jq -r .value.ready )" != "true" ]
-do
+while [[ "$( curl -s http://$HUB_HOST:4444/wd/hub/status | jq -r .value.ready )" != "true" ]]; do
 	sleep 1
 done
 
