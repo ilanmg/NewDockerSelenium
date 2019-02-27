07d7bcabd51dfdeed9e23ded9e1a4ed087680021
IlanMg <ilanmg@artlist.io>
2019-02-27 16:25:43 +0200

    finish

--- healthcheck.sh
+++ healthcheck.sh
@@ -4,9 +4,10 @@
 # BROWSER
 # MODULE
 
-echo "Checking if hub is ready now - $HUB_HOST"
+echo "Checking if hub is ready - $HUB_HOST"
 
-while [[ "$( curl -s http://$HUB_HOST:4444/wd/hub/status | jq -r .value.ready )" != "true" ]]; do
+while [ "$( curl -s http://$HUB_HOST:4444/wd/hub/status | jq -r .value.ready )" != "true" ]
+do
 	sleep 1
 done
 
@@ -15,4 +16,3 @@ java -cp selenium-docker.jar:selenium-docker-tests.jar:libs/* \
     -DHUB_HOST=$HUB_HOST \
     -DBROWSER=$BROWSER \
     org.testng.TestNG $MODULE
-
