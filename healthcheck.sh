f1594633f74804ba2ef96f1d3130e91b235ce5e8
IlanMg <ilanmg@artlist.io>
2019-02-27 09:59:25 +0200

    finish

--- healthcheck.sh
+++ healthcheck.sh
@@ -11,7 +11,6 @@ while [[ "$( curl -s http://$HUB_HOST:4444/wd/hub/status | jq -r .value.ready )"
 done
 
 # start the java command
-
 java -cp selenium-docker.jar:selenium-docker-tests.jar:libs/* \
     -DHUB_HOST=$HUB_HOST \
     -DBROWSER=$BROWSER \
